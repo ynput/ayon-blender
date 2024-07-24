04545fad4350601c5229270ae3bca72701e0bd9f
Kayla Man <moonyuet2008@hotmail.com>
2024-07-24 21:23:34 +0800

    improve publish validation error message and code tweaks

--- client/ayon_blender/plugins/publish/validate_absolute_external_data_paths.py
+++ client/ayon_blender/plugins/publish/validate_absolute_external_data_paths.py
@@ -1,4 +1,5 @@
 import bpy
+import inspect
 import pyblish.api
 from ayon_core.pipeline import (
     OptionalPyblishPluginMixin
@@ -15,12 +16,13 @@ class ValidateAbsoluteDataBlockPaths(pyblish.api.ContextPlugin,
     """Validates Absolute Data Block Paths
 
     This validator checks if all external data paths are absolute
-    to ensure the links would not be broke when publishing
+    to ensure the links would not be broken when publishing
     """
 
     label = "Validate Absolute Data Block Paths"
     order = ValidateContentsOrder
     hosts = ["blender"]
+    families = ["workfile"]
     optional = True
     actions = [RepairContextAction]
 
@@ -30,18 +32,19 @@ class ValidateAbsoluteDataBlockPaths(pyblish.api.ContextPlugin,
         object_type = type(bpy.data.objects)
         for attr in dir(bpy.data):
             collections = getattr(bpy.data, attr)
-            if isinstance(collections, object_type):
-                for data_block in collections:
-                    if not hasattr(data_block, "filepath"):
-                        continue
-                    if not data_block.filepath:
-                        continue
-                    if data_block.filepath == bpy.path.abspath(data_block.filepath):
-                        continue
+            if not isinstance(collections, object_type):
+                continue
+            for data_block in collections:
+                if not hasattr(data_block, "filepath"):
+                    continue
+                if not data_block.filepath:
+                    continue
+                if data_block.filepath == bpy.path.abspath(data_block.filepath):
+                    continue
 
-                    cls.log.error(f"Data block filepath {data_block.filepath} "
-                                    "is not absolute path")
-                    invalid.append(data_block.filepath)
+                cls.log.error(f"Data block filepath {data_block.filepath} "
+                                "is not absolute path")
+                invalid.append(data_block.filepath)
         return invalid
 
     def process(self, context):
@@ -53,12 +56,22 @@ class ValidateAbsoluteDataBlockPaths(pyblish.api.ContextPlugin,
             bullet_point_errors = "\n".join(
                 "- {}".format(error) for error in invalid
             )
-            report = (
-                "Data block filepaths are not absolute.\n\n"
-                f"{bullet_point_errors}\n\n"
-                "You can use repair action to fix it."
+            raise PublishValidationError(
+                "Invalid Data block filepaths",
+                title="Relative Data block filepaths",
+                description=self.get_description(bullet_point_errors)
             )
-            raise PublishValidationError(report, title="Relative Data block filepaths")
+
+    @classmethod
+    def get_description(cls, bullet_point_errors):
+        return inspect.cleandoc(f"""
+            ### Data block filepaths are invalid
+
+            Data block filepaths are not in absolute paths:
+            #### {bullet_point_errors}\n
+            You can use repair action to make the data block filepaths absolute.
+
+        """)
 
     @classmethod
     def repair(cls, context):
