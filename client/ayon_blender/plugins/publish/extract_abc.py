745d73acac0cb6b7232003bece4f67032c2e7687
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2024-08-29 23:16:00 +0800

    Update client/ayon_blender/plugins/publish/extract_abc.py

--- client/ayon_blender/plugins/publish/extract_abc.py
+++ client/ayon_blender/plugins/publish/extract_abc.py
@@ -48,7 +48,6 @@ class ExtractABC(plugin.BlenderExtractor, publish.OptionalPyblishPluginMixin):
             "frame_start": instance.data.get("frameStart"),
             "frame_end": instance.data.get("frameEnd"),
             "frame_step": instance.data.get("frameStep"),
-            "render.fps": instance.data.get("fps")
         }
         # Skip None value overrides
         scene_overrides = {
