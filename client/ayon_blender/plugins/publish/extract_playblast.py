013bc60fbcf120385fbde3de7c44470d5620f5cf
Roy Nieterau <roy_nieterau@hotmail.com>
2024-09-18 11:43:51 +0200

    Allow single frame playblast

--- client/ayon_blender/plugins/publish/extract_playblast.py
+++ client/ayon_blender/plugins/publish/extract_playblast.py
@@ -47,7 +47,7 @@ class ExtractPlayblast(
         end = instance.data.get("frameEnd", bpy.context.scene.frame_end)
 
         self.log.debug(f"start: {start}, end: {end}")
-        assert end > start, "Invalid time range !"
+        assert end >= start, "Invalid time range!"
 
         # get cameras
         camera = instance.data("review_camera", None)
