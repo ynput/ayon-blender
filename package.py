2b56291ae01cc54cc2f9937c237aa15f8dd2e661
Roy Nieterau <roy_nieterau@hotmail.com>
2024-10-07 22:58:35 +0200

    Define app host name

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "blender"
 title = "Blender"
 version = "0.2.1-dev.1"
-
+app_host_name = "blender"
 client_dir = "ayon_blender"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
