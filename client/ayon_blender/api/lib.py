5ffbfc4a053bbef2f55ddaafbb2e07ab52f6f75f
Kayla Man <moonyuet2008@hotmail.com>
2024-10-08 18:28:17 +0800

    code clean up on get_cache_modifiers

--- client/ayon_blender/api/lib.py
+++ client/ayon_blender/api/lib.py
@@ -566,28 +566,14 @@ def collect_animation_defs(create_context, step=True, fps=False):
 
 def get_cache_modifiers(obj, modifier_type="MESH_SEQUENCE_CACHE"):
     modifiers_dict = {}
-    modifiers = []
-    modifier_names = [modifier.name for modifier in obj.modifiers
-                      if modifier.type == modifier_type]
-    if modifier_names:
-        modifiers = [modifier for modifier in obj.modifiers
-                     if modifier.type == modifier_type]
-        modifiers_dict = {
-            obj.name: (
-                modifier for modifier in obj.modifiers
-                if modifier.type == modifier_type
-            )
-        }
+    modifiers = [modifier for modifier in obj.modifiers
+                 if modifier.type == modifier_type]
+    if modifiers:
+        modifiers_dict[obj.name] = modifiers
     else:
         for sub_obj in obj.children:
             for ob in sub_obj.children:
                 cache_modifiers = [modifier for modifier in ob.modifiers
                                    if modifier.type == modifier_type]
-                modifiers.extend(cache_modifiers)
-                modifiers_dict.update({
-                    ob.name: (
-                        modifier for modifier in ob.modifiers
-                        if modifier.type == modifier_type
-                    )
-                })
+                modifiers_dict[ob.name] = cache_modifiers
     return modifiers_dict
