0491b197a220dfc848f34033211c1f018a8e6b8b
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2024-12-10 20:21:02 +0800

    Update client/ayon_blender/plugins/create/create_blendScene.py
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- client/ayon_blender/plugins/create/create_blendScene.py
+++ client/ayon_blender/plugins/create/create_blendScene.py
@@ -37,12 +37,6 @@ class CreateBlendScene(plugin.BlenderCreator):
 
         for instance in instances:
             node = instance.transient_data["instance_node"]
-
-            if isinstance(node, bpy.types.Collection):
-                for children in node.children_recursive:
-                    if isinstance(children, bpy.types.Collection):
-                        bpy.data.collections.remove(children)
-                    else:
-                        bpy.data.objects.remove(children)
+            bpy.data.collections.remove(node)
 
             self._remove_instance_from_context(instance)
