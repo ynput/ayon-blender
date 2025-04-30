da5054326d16ada1a99cee646c15d36d4e32a2bf
Kayla Man <moonyuet2008@hotmail.com>
2025-04-30 17:38:20 +0800

    updating and removing instances for overriding option should be working

--- client/ayon_blender/plugins/load/load_blend_link.py
+++ client/ayon_blender/plugins/load/load_blend_link.py
@@ -1,5 +1,5 @@
 from typing import Dict, List, Optional
-
+import os
 import bpy
 
 from ayon_core.lib import BoolDef
@@ -57,7 +57,8 @@ class BlendLinkLoader(plugin.BlenderLoader):
             name, namespace, context, suffix="CON"
         )
         loaded_collection = bpy.data.collections.get(container_name)
-        if loaded_collection:
+        scene_collection = bpy.context.scene.collection
+        if loaded_collection and container_name in scene_collection.children:
             self.log.debug(f"Collection {container_name} already loaded.")
             return
 
@@ -87,7 +88,11 @@ class BlendLinkLoader(plugin.BlenderLoader):
             add_asset_to_group(context["folder"], loaded_collection)
         # TODO: Store loader options for later use (e.g. on update)
         # Store the loader options on the container for later use if needed.
-        metadata_update(container_collection, {"options": options})
+        extra_data =  {
+            "options": options,
+            "lib_name": os.path.basename(filepath)
+        }
+        metadata_update(container_collection, extra_data)
 
         return (container_collection, loaded_collection)
 
@@ -96,20 +101,29 @@ class BlendLinkLoader(plugin.BlenderLoader):
         repre = context["representation"]
         collection = container["node"]
         new_filepath = self.filepath_from_context(context)
+        new_filename = os.path.basename(new_filepath)
         # Update library filepath and reload it
-        library = self._get_library_from_collection(collection)
+        library = (
+            self._get_library_from_collection(collection)
+            or self._get_library_by_name(container)
+        )
+        library.name = new_filename
         library.filepath = new_filepath
         library.reload()
+
         # refresh UI
         bpy.context.view_layer.update()
         # Update container metadata
-        metadata_update(collection, {"representation": str(repre["id"])})
+        updated_data = {
+            "representation": str(repre["id"]),
+            "lib_name": new_filename
+        }
+        metadata_update(collection, updated_data)
 
     def exec_remove(self, container: Dict) -> bool:
         """Remove existing container from the Blender scene."""
         collection = container["node"]
         library = self._get_library_from_collection(collection)
-        # TODO: make sure it also works with overriding option enabled
         if library:
             bpy.data.libraries.remove(library)
         else:
@@ -133,3 +147,10 @@ class BlendLinkLoader(plugin.BlenderLoader):
             return child.override_library.reference.library
 
         return None
+
+    def _get_library_by_name(self, container: Dict) -> bpy.types.Library:
+        """Get the library by filename."""
+        lib_name = container["lib_name"]
+        for library in bpy.data.libraries:
+            if lib_name in library.name:
+                return library
