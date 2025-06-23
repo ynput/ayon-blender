00a22b2537faa61dd42631e26ca6906176f3757f
Kayla Man <moonyuet2008@hotmail.com>
2025-06-23 19:52:35 +0800

    support to check on the bpy.types.Node when striping namespace

--- client/ayon_blender/api/lib.py
+++ client/ayon_blender/api/lib.py
@@ -621,7 +621,7 @@ def strip_namespace(containers):
             children = node.children_recursive
         elif isinstance(node, bpy.types.Object):
             children = node.children
-        elif isinstance(node, bpy.types.ShaderNodeTexImage):
+        elif isinstance(node, bpy.types.Node):
             children = [node]
         else:
             raise TypeError(f"Unsupported type: {node} ({type(node)})")
