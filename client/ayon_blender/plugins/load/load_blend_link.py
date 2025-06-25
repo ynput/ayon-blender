c664cdbe176a02f1ad760dd494307600fceb1a32
Kayla Man <moonyuet2008@hotmail.com>
2025-06-25 19:06:25 +0800

    clean up the code and add comment for updating function

--- client/ayon_blender/plugins/load/load_blend_link.py
+++ client/ayon_blender/plugins/load/load_blend_link.py
@@ -113,16 +113,13 @@ class BlendLinkLoader(plugin.BlenderLoader):
         repre = context["representation"]
         collection = container["node"]
         new_filepath = self.filepath_from_context(context)
-        # Update library filepath and reload it
-        library = self._get_or_build_library_by_path(new_filepath)
-        # The current updating function only supports single asset
-        # versioning only due to the blender API limitation on removing
-        # the old and unused datablock right after reloading the new library
-        # and replace it with the old one.
+        # Work for updating multiple assets in blender 4.5
+        # Otherwise, it only works for updating single assets
+        # in blender 4.4
         for node in collection.children:
             if node.library:
-                node.library.name = library.name
-                node.library.filepath = library.filepath
+                node.library.name = os.path.basename(new_filepath)
+                node.library.filepath = new_filepath
                 node.library.reload()
         # refresh UI
         bpy.context.view_layer.update()
@@ -180,25 +177,3 @@ class BlendLinkLoader(plugin.BlenderLoader):
         )
 
         return len(match_count) > 1
-
-    def _get_or_build_library_by_path(self, libpath: str) -> bpy.types.Library:
-        """Get the library by filepath. If there is no any
-        associated library to the path, the related library is loaded
-        accordingly."""
-        for library in bpy.data.libraries:
-            if libpath == os.path.normpath(library.filepath):
-                return library
-
-        with bpy.data.libraries.load(libpath, link=True, relative=False) as (
-            data_from,
-            data_to
-        ):
-            if data_from.collections:
-                data_to.collections = data_from.collections
-            elif data_from.objects:
-                data_to.objects = data_from.objects
-
-        # Return the newly loaded library
-        for library in bpy.data.libraries:
-            if libpath == os.path.normpath(library.filepath):
-                return library
