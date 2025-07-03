eaf249ebb36d7fd0ab0346f53f7947d733b2a023
MustafaJafar <mustafataherzaky@outlook.com>
2025-07-03 17:15:18 +0300

    Add `Create asset hierarchy` in `USD` creator

--- client/ayon_blender/plugins/create/create_usd.py
+++ client/ayon_blender/plugins/create/create_usd.py
@@ -1,5 +1,7 @@
 """Create a USD Export."""
 
+import bpy
+from ayon_core.lib import BoolDef
 from ayon_blender.api import plugin, lib
 
 
@@ -20,11 +22,48 @@ class CreateUSD(plugin.BlenderCreator):
             product_name, instance_data, pre_create_data
         )
 
+        objects = []
         if pre_create_data.get("use_selection"):
             objects = lib.get_selection()
+
+        # Create template hierarchy
+        if pre_create_data.get("createAssetTemplateHierarchy", False):
+            folder_path = instance_data["folderPath"]
+            folder_name = folder_path.rsplit("/", 1)[-1]
+
+            root = bpy.data.objects.new(folder_name, object_data=None)
+            bpy.context.scene.collection.objects.link(root)
+
+            geo = bpy.data.objects.new("geo", object_data=None)
+            bpy.context.scene.collection.objects.link(geo)
+            geo.parent = root
+
+            # Parent members with geo and remove them from selection
             for obj in objects:
-                collection.objects.link(obj)
-                if obj.type == 'EMPTY':
-                    objects.extend(obj.children)
+                obj.parent = geo
+                obj.select_set(False)
+
+            # Select root
+            root.select_set(True)
+            objects = [root]      
+
+        for obj in objects:
+            collection.objects.link(obj)
+            if obj.type == 'EMPTY':
+                objects.extend(obj.children)
 
         return collection
+
+    def get_pre_create_attr_defs(self):
+        defs = super().get_pre_create_attr_defs()
+        defs.extend([
+            BoolDef("createAssetTemplateHierarchy",
+                    label="Create asset hierarchy",
+                    tooltip=(
+                        "Create the root hierarchy for '{folder_name}/geo'"
+                        " as per the USD Asset Structure guidelines to"
+                        " add your geometry into."
+                    ),
+                    default=False)
+        ])
+        return defs
