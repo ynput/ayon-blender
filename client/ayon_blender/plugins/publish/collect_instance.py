5108b4a20a23ce3c7c9190545a0fd84c535bef31
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-04 14:19:12 +0200

    Collect full hierarchy of objects inside a collection as part of the instance, even if they are not directly linked as objects to the collection.
    This way, if someone only adds the root group in the publish collection, the resulting publish will still include all children.

--- client/ayon_blender/plugins/publish/collect_instance.py
+++ client/ayon_blender/plugins/publish/collect_instance.py
@@ -8,7 +8,18 @@ from ayon_blender.api.pipeline import AVALON_PROPERTY
 
 
 class CollectBlenderInstanceData(plugin.BlenderInstancePlugin):
-    """Validator to verify that the instance is not empty"""
+    """Collect members of the instance.
+
+    For a Collection this includes itself and all directly linked objects and
+    their full hierarchy of children objects. It also includes direct child
+    collections. It does *not* include objects or collections from collections
+    inside the Collection (it does not recurse into nested collections).
+
+    For an Object (e.g. instance asset group) this includes all its children
+    hierarchy.
+
+    These members are then set on the instance as a list of objects.
+    """
 
     order = pyblish.api.CollectorOrder
     hosts = ["blender"]
@@ -20,25 +31,32 @@ class CollectBlenderInstanceData(plugin.BlenderInstancePlugin):
         instance_node = instance.data["transientData"]["instance_node"]
 
         # Collect members of the instance
-        members = [instance_node]
+        members: set[str] = {instance_node}
+        self.log.debug(f"Found instance node: {instance_node}")
         if isinstance(instance_node, bpy.types.Collection):
-            members.extend(instance_node.objects)
-            members.extend(instance_node.children)
+            # Note that for a `bpy.types.Collection` the `children` and
+            # `children_recursive` only include child collections, not objects.
+            # To get the linked objects (and their children) we first collect
+            # the objects, and then add all their `children_recursive`.
+            objects = list(instance_node.objects)
+            members.update(objects)
+            for obj in objects:
+                members.update(obj.children_recursive)
 
             # Special case for animation instances, include armatures
             if instance.data["productType"] == "animation":
                 for obj in instance_node.objects:
                     if obj.type == 'EMPTY' and obj.get(AVALON_PROPERTY):
-                        members.extend(
+                        members.update(
                             child for child in obj.children
                             if child.type == 'ARMATURE'
                         )
         elif isinstance(instance_node, bpy.types.Object):
-            members.extend(instance_node.children_recursive)
+            members.update(instance_node.children_recursive)
         else:
             raise KnownPublishError(
                 f"Unsupported instance node type '{type(instance_node)}' "
                 f"for instance '{instance}'"
             )
 
-        instance[:] = members
+        instance[:] = list(members)
