693586ce84c1bf3bb64d2c803b9267caf2857c37
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-11 22:53:54 +0200

    Make Including Objects Child Hierarchies for Collections optional behavior

--- client/ayon_blender/plugins/publish/collect_instance.py
+++ client/ayon_blender/plugins/publish/collect_instance.py
@@ -3,12 +3,15 @@ import bpy
 
 import pyblish.api
 
+from ayon_core.lib import BoolDef
+from ayon_core.pipeline import AYONPyblishPluginMixin
 from ayon_core.pipeline.publish import KnownPublishError
 from ayon_blender.api import plugin
 from ayon_blender.api.pipeline import AVALON_PROPERTY
 
 
-class CollectBlenderInstanceData(plugin.BlenderInstancePlugin):
+class CollectBlenderInstanceData(plugin.BlenderInstancePlugin,
+                                 AYONPyblishPluginMixin):
     """Collect members of the instance.
 
     For a Collection this includes itself and all directly linked objects and
@@ -35,15 +38,21 @@ class CollectBlenderInstanceData(plugin.BlenderInstancePlugin):
         members: set[str] = {instance_node}
         self.log.debug(f"Found instance node: {instance_node}")
         if isinstance(instance_node, bpy.types.Collection):
-            # Add all child objects (hierarchy)
+            # Add all directly linked objects
+            objects = list(instance_node.objects)
+            members.update(objects)
+
+            # Add all object children recursively (hierarchy)
             # Note that for a `bpy.types.Collection` the `children` and
             # `children_recursive` only include child collections, not objects.
             # To get the linked objects (and their children) we first collect
             # the objects, and then add all their `children_recursive`.
-            objects = list(instance_node.objects)
-            members.update(objects)
-            for obj in objects:
-                members.update(obj.children_recursive)
+            attr_values = self.get_attr_values_from_data(instance.data)
+            if attr_values.get(
+                "collection_include_object_children_recursive", True
+            ):
+                for obj in objects:
+                    members.update(obj.children_recursive)
 
             # Add child collections
             members.update(instance_node.children)
@@ -65,3 +74,30 @@ class CollectBlenderInstanceData(plugin.BlenderInstancePlugin):
             )
 
         instance[:] = list(members)
+
+    @classmethod
+    def get_attr_defs_for_instance(
+        cls, create_context, instance
+    ):
+        if not cls.instance_matches_plugin_families(instance):
+            return []
+
+        # If the instance is a collection, we provide an optional option
+        # to include the children hierarchy of objects in the collection.
+        instance_node = instance.transient_data.get("instance_node", None)
+        if not isinstance(instance_node, bpy.types.Collection):
+            return []
+
+        return [
+            BoolDef(
+                "collection_include_object_children_recursive",
+                label="Include Objects Hierarchy",
+                default=True,
+                tooltip=(
+                    "If enabled, the children of objects in the collection "
+                    "will be included in the instance members.\n"
+                    "If disabled, only objects directly linked to the "
+                    "collection will be included."
+                )
+            )
+        ]
