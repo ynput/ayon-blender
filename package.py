e90ed4d5b3b75d0656099e165e320e1aab07306c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-07-31 15:21:46 +0200

    allow per project addon version

--- package.py
+++ package.py
@@ -3,6 +3,7 @@ title = "Blender"
 version = "1.0.1+dev"
 app_host_name = "blender"
 client_dir = "ayon_blender"
+project_can_override_addon_version = True
 
 ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
