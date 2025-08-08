1782c4a7fb605a94385777e2630952a1aa872537
Kayla Man <moonyuet2008@hotmail.com>
2025-08-08 22:56:51 +0800

    clean up

--- client/ayon_blender/plugins/publish/collect_temporary_files_for_clean_up.py
+++ client/ayon_blender/plugins/publish/collect_temporary_files_for_clean_up.py
@@ -1,26 +1,24 @@
 import os
 import pyblish.api
+import bpy
 
+class CollectTemporaryFilesCleanUp(pyblish.api.InstancePlugin):
+    """Collect Scene Render Temporary Files for the later clean up."""
 
-class CollectTemporaryFilesForCleanUp(pyblish.api.InstancePlugin):
-    """Compare rendered and expected files"""
-
-    label = "Collect Temporary Files for Clean Up"
+    label = "Collect Scene Render Temporary Files"
     order = pyblish.api.CollectorOrder - 0.1
     hosts = ["blender"]
     targets = ["farm"]
 
 def process(self, instance):
-    for repre in instance.data["representations"]:
-        staging_dir = repre["stagingDir"]
-        temp_dir = os.path.join(staging_dir, "tmp")
-        if not os.path.exists(temp_dir):
-            self.log.debug(f"Temporary directory does not exist: {temp_dir}")
-            continue
-        self.log.debug(f"Collecting files for cleanup in directory: {temp_dir}")
-        for file in os.listdir(temp_dir):
-            file_path = os.path.join(temp_dir, file)
-            if os.path.isfile(file_path):
-                self.log.debug(f"Adding file to cleanup: {file_path}")
-                instance.context.data["cleanupFullPaths"].append(file_path)
-        instance.context.data["cleanupEmptyDirs"].append(temp_dir)
+    temp_dir = bpy.context.scene.render.filepath
+    if not os.path.exists(temp_dir):
+        self.log.debug(f"Temporary directory does not exist: {temp_dir}")
+        return
+    self.log.debug(f"Collecting files for cleanup in directory: {temp_dir}")
+    for file in os.listdir(temp_dir):
+        file_path = os.path.join(temp_dir, file)
+        if os.path.isfile(file_path):
+            self.log.debug(f"Adding file to cleanup: {file_path}")
+            instance.context.data["cleanupFullPaths"].append(file_path)
+    instance.context.data["cleanupEmptyDirs"].append(temp_dir)
