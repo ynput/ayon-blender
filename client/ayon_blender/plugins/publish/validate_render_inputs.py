17cb2c01565bbd0e11ac33cb5be64111ffd2e033
Roy Nieterau <roy_nieterau@hotmail.com>
2025-08-26 10:15:57 +0200

    Validate Render Inputs: Improve validation report

--- client/ayon_blender/plugins/publish/validate_render_inputs.py
+++ client/ayon_blender/plugins/publish/validate_render_inputs.py
@@ -1,3 +1,5 @@
+import inspect
+
 import bpy
 
 import pyblish.api
@@ -20,12 +22,38 @@ class ValidateRenderCompositorNodeFileOutputConnected(
     hosts = ["blender"]
     families = ["render"]
     label = "Validate Render Inputs"
+    # TODO: Not sure how to select a Compositor Node through Python API so
+    #       until then, we can't select the node via the UI.
+    # actions = [SelectInvalidAction]
     optional = True
 
     def process(self, instance):
         if not self.is_active(instance.data):
             return
 
+        invalid = self.get_invalid(instance)
+
+        if invalid:
+
+            node = invalid[0].node
+            node_name = node.name
+
+            labels = []
+            for socket in invalid:
+                label = socket.name
+                labels.append(label)
+
+            raise PublishValidationError(
+                f"The Compositor File Output Node '{node_name}' has the "
+                "following unconnected image slots:\n{}".format(
+                    "\n".join(f"- {label}" for label in labels)
+                ),
+                title="Unconnected image slots",
+                description=self.get_description(),
+            )
+
+    @classmethod
+    def get_invalid(cls, instance):
         output: bpy.types.CompositorNodeOutputFile = (
             instance.data["transientData"]["instance_node"]
         )
@@ -39,10 +67,13 @@ class ValidateRenderCompositorNodeFileOutputConnected(
                 if not input_.links:
                     invalid.append(input_)
 
-        if invalid:
-            raise PublishValidationError(
-                "The Compositor File Output Node has the following "
-                "unconnected image slots: {}".format(
-                    ", ".join(str(socket) for socket in invalid)
-                )
-            )
+        return invalid
+
+    @staticmethod
+    def get_description():
+        return inspect.cleandoc("""### Unconnected image slots
+        
+        The Compositor File Output Node has unconnected input image slots.
+        Make sure to connect each of the individual slots to an input, or
+        remove the irrelevant slots if they are not needed.
+        """)
