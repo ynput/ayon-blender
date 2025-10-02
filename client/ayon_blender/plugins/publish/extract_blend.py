4cc4a78c795c7894a9da5421b69e165d836c249e
Kayla Man <moonyuet2008@hotmail.com>
2025-10-02 20:56:00 +0800

    remove condition

--- client/ayon_blender/plugins/publish/extract_blend.py
+++ client/ayon_blender/plugins/publish/extract_blend.py
@@ -172,10 +172,7 @@ class ExtractBlendAction(ExtractBlend):
                     if not data.animation_data:
                         data.animation_data_create()
                     data.animation_data.action = child.animation_data.action
-                    if child.animation_data.action.name.startswith(
-                        instance.data["productName"]
-                    ):
-                        data_blocks.add(child.animation_data.action)
                     data.animation_data_clear()
+                    data_blocks.add(child.animation_data.action)
 
         return data_blocks
