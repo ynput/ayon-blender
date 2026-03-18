7629166fbbe5e7e15b044238cbdd708ca622a404
Roy Nieterau <roy_nieterau@hotmail.com>
2026-03-18 13:38:42 +0100

    Validate also both state for view layers

--- client/ayon_blender/plugins/publish/validate_renderlayer_active.py
+++ client/ayon_blender/plugins/publish/validate_renderlayer_active.py
@@ -24,18 +24,33 @@ class ValidateRenderlayerActive(plugin.BlenderContextPlugin):
     label = "Validate Renderlayer Active"
     actions = [RepairContextAction]
 
-    @staticmethod
-    def get_required_viewlayers(context: pyblish.api.Context) -> set[str]:
-        """Get the set of required view layers based on the instance's view
-        layer definitions.
+    def process(self, context: pyblish.api.Context) -> None:
+        """
+        Validate the state of view layers based on the instance's view layer
 
         Args:
             context (pyblish.api.Context): Context data
-
-        Returns:
-            set[str]: A set of required view layer nodes.
         """
-        required_viewlayer_nodes = set()
+        enabled_compositor_nodes = self.get_enabled_compositor_nodes(context)
+        if not enabled_compositor_nodes:
+            # No render instances enabled, nothing to validate
+            return
+
+        invalid = self.get_invalid(enabled_compositor_nodes)
+        if invalid:
+            raise PublishValidationError(
+                "Some view layers are not in the expected state.",
+                title="Invalid view layer states",
+                description=self.get_description(),
+            )
+
+    @staticmethod
+    def get_enabled_compositor_nodes(
+        context: pyblish.api.Context
+    ) -> set["bpy.types.CompositorNodeOutputFile"]:
+        """Return Compositor File Output nodes that are associated with
+        enabled render instances."""
+        nodes = set()
         for instance in context:
             # Process only render instances
             if instance.data["productBaseType"] != "render":
@@ -46,47 +61,43 @@ class ValidateRenderlayerActive(plugin.BlenderContextPlugin):
             comp_output_node: "bpy.types.CompositorNodeOutputFile" = (
                 instance.data["transientData"]["instance_node"]
             )
-            used_viewlayers = lib.get_upstream_viewlayers(comp_output_node)
-            required_viewlayer_nodes.update(used_viewlayers)
-
-        return required_viewlayer_nodes
-
-    def process(self, context: pyblish.api.Context) -> None:
-        """
-        Validate the state of view layers based on the instance's view layer
-
-        Args:
-            context (pyblish.api.Context): Context data
-        """
-        invalid = self.get_invalid(context)
-        if invalid:
-            raise PublishValidationError(
-                "Some view layers are not in the expected state.",
-                title="Invalid view layer states",
-                description=self.get_description(),
-            )
+            nodes.add(comp_output_node)
+        return nodes
 
     @classmethod
     def get_invalid(
-        cls, context: pyblish.api.Context) -> list[bpy.types.ViewLayer]:
+        cls,
+        enabled_compositor_nodes: set["bpy.types.CompositorNodeOutputFile"]
+    ) -> list[bpy.types.ViewLayer]:
         """Get the list of invalid view layers based on the instance's view
         layer definitions.
 
         Args:
-            context (pyblish.api.Context): Context data
+            enabled_compositor_nodes: Compositor File Output nodes that
+                are associated with enabled render instances.
 
         Returns:
             list[bpy.types.ViewLayer]: A list of invalid view layers.
         """
+
+        required_viewlayers = set()
+        for comp_output_node in enabled_compositor_nodes:
+            used_viewlayers = lib.get_upstream_viewlayers(comp_output_node)
+            required_viewlayers.update(used_viewlayers)
+
         invalid = []
-        required_viewlayers = cls.get_required_viewlayers(context)
         for viewlayer in bpy.context.scene.view_layers:
             is_required = viewlayer.name in required_viewlayers
-            if is_required and not viewlayer.use:
+            if viewlayer.use != is_required:
+                active_label = "active" if viewlayer.use else "inactive"
+                required_label = "active" if is_required else "inactive"
+
                 cls.log.error(
-                    f"View layer '{viewlayer.name}' is expected to be active but is inactive."
+                    f"View layer '{viewlayer.name}' is expected to"
+                    f" be {required_label} but is {active_label}."
                 )
                 invalid.append(viewlayer)
+
         return invalid
 
     @classmethod
@@ -100,7 +111,8 @@ class ValidateRenderlayerActive(plugin.BlenderContextPlugin):
         """
         invalid = cls.get_invalid(context)
         for viewlayer in invalid:
-            viewlayer.use = True
+            # Toggle from the invalid state
+            viewlayer.use = not viewlayer.use
 
     def get_description(self):
         return inspect.cleandoc(
