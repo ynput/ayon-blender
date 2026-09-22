ea041579c5c3676cb30c7e3b6ea2bf861d10d489
Kayla Man <moonyuet2008@hotmail.com>
2026-09-22 14:53:26 +0800

    fix syntax error

--- server/settings/publish_playblast.py
+++ server/settings/publish_playblast.py
@@ -222,7 +222,7 @@ class ExtractPlayblastModel(BaseSettingsModel):
 
 DEFAULT_PLAYBLAST_SETTING = {
     "enabled": True,
-    "optional": True
+    "optional": True,
     "active": True,
     "presets": json.dumps(
         {
